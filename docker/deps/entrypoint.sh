d14aa8af0ad56fe49b781afabf3233bb32d75bf4
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2024-01-17 00:22:31 -0500

    refactor dockerfile

---
+++ docker/deps/entrypoint.sh
@@ -0,0 +1,10 @@
+source /root/setup_docker_env.sh
+
+# Run the CMD passed as command-line arguments or keep container open.
+if [ $# -eq 0 ]; then
+  echo "Starting container and waiting forever..."
+  exec sleep inf
+else
+  echo "Running command: $@"
+  "$@"
+fi
